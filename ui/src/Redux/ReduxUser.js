03a34c66c42b2be2039242e98c2de12ce404c479
Bishal Poudel <poudelb172@gmail.com>
2021-11-19 22:25:37 +0545

    user logout made

--- ui/src/Redux/ReduxUser.js
+++ ui/src/Redux/ReduxUser.js
@@ -22,6 +22,7 @@ const userSlice = createSlice(
             },
             logoutUser: (state) => {
                 state.curruser = null;
+                localStorage.setItem('user',null)
             },
         }
     }
