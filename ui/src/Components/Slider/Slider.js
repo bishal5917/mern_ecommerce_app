bae60a67d1524a6611bac6e4fd7664fec55ef2ff
Bishal Poudel <poudelb172@gmail.com>
2021-12-11 14:31:16 +0545

    sliders Introduced

--- ui/src/Components/Slider/Slider.js
+++ ui/src/Components/Slider/Slider.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './slider.css'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 export default function Slider() {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+
+    const leftTransform = () => {
+        setLeft(-1)
+    }
+
+    const rightTransform = () => {
+
+    }
     return (
         <>
             <div className="sliderImages">
-                <ChevronLeftIcon style={{"fontSize":"40"}} 
-                className="left"/>
-                <ChevronRightIcon style={{"fontSize":"40"}}
-                 className="right"/>
-                <img className='sliderImage'
-                    src="https://itti.com.np/pub/media/wysiwyg/itti_sliders/Acer-Predator-Triton-500-2020-final.jpg" alt="" srcset="" />
+                <ChevronLeftIcon onClick={leftTransform} style={{ "fontSize": "40" }}
+                    className="left" />
+                <ChevronRightIcon onClick={rightTransform} style={{ "fontSize": "40" }}
+                    className="right" />
+                <div className="sliderImgs">
+                    <img className='sliderImage'
+                        src="slider1.jpg" alt="" srcset="" />
+                    <img className='sliderImage'
+                        src="slider2.jpg" alt="" srcset="" />
+                    <img className='sliderImage'
+                        src="slider3.jpg" alt="" srcset="" />
+                </div>
             </div>
         </>
     )
