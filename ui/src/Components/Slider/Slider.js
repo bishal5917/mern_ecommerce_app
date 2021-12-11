df659cc338a81c949e0cebdce2f0e01fdc1b7fc3
Bishal Poudel <poudelb172@gmail.com>
2021-12-11 16:48:25 +0545

    slider made

--- ui/src/Components/Slider/Slider.js
+++ ui/src/Components/Slider/Slider.js
@@ -2,33 +2,43 @@ import React, { useState } from 'react'
 import './slider.css'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import styled from 'styled-components'
+
+const SliderDiv = styled.div`
+
+display: flex;
+transition:0.2s ease-out;
+overflow-x:hidden;
+transform:translateX(${props => props.slideby * -100}vw)
+
+`
 
 export default function Slider() {
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
+    const [slideby, setSlideBy] = useState(0)
 
-    const leftTransform = () => {
-        setLeft(-1)
-    }
-
-    const rightTransform = () => {
-
+    const handleClick = (slideDirection) => {
+        if (slideDirection === "left") {
+            setSlideBy(slideby > 0 ? slideby - 1 : 2)
+        }
+        else {
+            setSlideBy(slideby < 2 ? slideby + 1 : 0)
+        }
     }
     return (
         <>
             <div className="sliderImages">
-                <ChevronLeftIcon onClick={leftTransform} style={{ "fontSize": "40" }}
-                    className="left" />
-                <ChevronRightIcon onClick={rightTransform} style={{ "fontSize": "40" }}
-                    className="right" />
-                <div className="sliderImgs">
+                <SliderDiv slideby={slideby}>
                     <img className='sliderImage'
                         src="slider1.jpg" alt="" srcset="" />
                     <img className='sliderImage'
                         src="slider2.jpg" alt="" srcset="" />
                     <img className='sliderImage'
                         src="slider3.jpg" alt="" srcset="" />
-                </div>
+                </SliderDiv>
+                <ChevronLeftIcon onClick={() => handleClick("left")} style={{ "fontSize": "40" }}
+                    className="left" />
+                <ChevronRightIcon onClick={() => handleClick("right")} style={{ "fontSize": "40" }}
+                    className="right" />
             </div>
         </>
     )
