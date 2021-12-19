f02c997ade48695a9c9d6593c43793854581ad62
Bishal Poudel <poudelb172@gmail.com>
2021-12-19 20:10:04 +0545

    review updated

--- api/routes/reviews.js
+++ api/routes/reviews.js
@@ -42,4 +42,22 @@ router.delete('/delete/:rid', async (req, res) => {
 })
 
 
+//edit a review
+router.put('/edit/:rid', async (req, res) => {
+    const foundReview = await Review.findById(req.params.rid)
+    const reviewer = foundReview.reviewerId
+    if (req.body.userId === reviewer) {
+        try {
+            const updatedReview = await Review.findByIdAndUpdate(req.params.rid, {
+                $set: req.body
+            }, { new: true })
+            res.status(200).json(updatedReview)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+    else {
+        res.status(403).json("You are Forbidden")
+    }
+})
 module.exports = router
